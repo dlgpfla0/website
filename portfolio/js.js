79ad19bbe4f8fbd73056d480e9a451c2599aa844
dlgpfla0 <39292329+dlgpfla0@users.noreply.github.com>
2018-11-06 16:10:31 +0900

    portfolio1

--- portfolio/js.js
+++ portfolio/js.js
@@ -1,24 +1,51 @@
 $(document).ready(function(){
-    var menu=document.querySelector(".btnFrame .btn-inner"),
-        container=document.querySelector("#Wrap");
-    
-    menu.addEventListener("click", function(){
-        this.classList.toggle("open");
-        container.classList.toggle("active");
+    $("#gnb .gnb_about").click(function(){
+        var position=$("section.about").offset();
+        $("body").stop().animate({scrollTop:position.top}, 500);
+    });
+    $("#gnb .gnb_ability").click(function(){
+        var position=$(".ability").offset();
+        $("body").stop().animate({scrollTop:position.top}, 500);
     });
 });
 
 
+
+/* portfolio tab 구현 */
 $(document).ready(function(){
     $(".tab_content").hide();
     $(".tab_content:first").show();
     
     $("ul.tabs li").click(function(){
-        $("ul.tabs li").removeClass("active").css("color", "#333");
-        
-        $(this).addClass("active").css("color", "darkred");
-        $(".tab_content").hide();
+        $("ul.tabs li").removeClass("active").css("color", "#acacac");
+        $(this).addClass("active").css("color", "#000");
+        $(".tab_content").hide()
         var activeTab=$(this).attr("rel");
         $("#" + activeTab).fadeIn();
     });
 });
+
+
+/* 팝업 구현 */
+$(document).ready(function(){
+    cookiedata=document.cookie;
+    if (cookiedata.indexOf("maindiv=done") < 0) {
+        document.all['divpop'].style.visibility = "visible";
+    }
+    else {
+        document.all['divpop'].style.visibility = "hidden";
+    }
+    
+    function setCookie( name, value, expiredays) {
+        var todayDate = new Date();
+        todayDate.setDate( todayDate.getDate() + expiredays );
+        document.cookie = name + "=" + escape( value ) + "; path=/; expires=" + todayDate.toGMTString() + ";"
+    }
+    $(".popborder .close").click(function(){
+        $("#divpop").fadeOut(500, function(){
+            $(this).find(".popborder").empty();
+        });
+    });
+});
+
+/* 타이핑 */
---
+++ portfolio/js/popup.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    cookiedata=document.cookie;
+    if (cookiedata.indexOf("maindiv=done") < 0){
+        document.all['divpop'].style.visibility = "visible";
+    }
+    else {
+        document.all['divpop'].style.visibility = "hidden";
+    }
+    
+    function setCookie( name, value, expiredays) {
+        var todayDate = new Date();
+        todayDate.setDate( todayDate.getDate() + expiredays);
+        document.cookie = name + "=" + escape( value ) + "; path=/; expires=" + todayDate.toGMTString() + ";"
+    }
+    function closeWin() {
+        if (document.notice_form.ckBox.checked) {
+            setCookie( "maindiv", "done", 1 );
+        }
+        document.all['divpop'].style.visibility = "hidden";
+    }
+});
