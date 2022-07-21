42f459b43e3f57b1a51e4f438367cffeff564fd1
dlgpfla0 <39292329+dlgpfla0@users.noreply.github.com>
2022-07-21 12:26:41 +0900

    Add files via upload

---
+++ portfolio/js/port_pop.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){
+    $(".icon-links.web1 a").click(function(){
+        $(".port-pop.web1").fadeIn();
+    });
+    $(".port-pop.web1 .close-btn").click(function(){
+        $(".port-pop.web1").fadeOut();
+    });
+    
+    $(".icon-links.web2 a").click(function(){
+        $(".port-pop.web2").fadeIn();
+    });
+    $(".port-pop.web2 .close-btn").click(function(){
+        $(".port-pop.web2").fadeOut();
+    });
+    
+    $(".icon-links.web3 a").click(function(){
+        $(".port-pop.web3").fadeIn();
+    });
+    $(".port-pop.web3 .close-btn").click(function(){
+        $(".port-pop.web3").fadeOut();
+    });
+    
+    $(".icon-links.web4 a").click(function(){
+        $(".port-pop.web4").fadeIn();
+    });
+    $(".port-pop.web4 .close-btn").click(function(){
+        $(".port-pop.web4").fadeOut();
+    });
+    
+    $(".icon-links.web5 a").click(function(){
+        $(".port-pop.web5").fadeIn();
+    });
+    $(".port-pop.web5 .close-btn").click(function(){
+        $(".port-pop.web5").fadeOut();
+    });
+    
+    $(".icon-links.web6 a").click(function(){
+        $(".port-pop.web6").fadeIn();
+    });
+    $(".port-pop.web6 .close-btn").click(function(){
+        $(".port-pop.web6").fadeOut();
+    });
+    
+    $(".icon-links.web7 a").click(function(){
+        $(".port-pop.web7").fadeIn();
+    });
+    $(".port-pop.web7 .close-btn").click(function(){
+        $(".port-pop.web7").fadeOut();
+    });
+    
+    $(".icon-links.web8 a").click(function(){
+        $(".port-pop.web7").fadeIn();
+    });
+    $(".port-pop.web8 .close-btn").click(function(){
+        $(".port-pop.web8").fadeOut();
+    });
+    
+    $(".icon-links.mo1 a").click(function(){
+        $(".port-pop.mo1").fadeIn();
+    });
+    $(".port-pop.mo1 .close-btn").click(function(){
+        $(".port-pop.mo1").fadeOut();
+    });
+    
+    $(".icon-links.mo2 a").click(function(){
+        $(".port-pop.mo2").fadeIn();
+    });
+    $(".port-pop.mo2 .close-btn").click(function(){
+        $(".port-pop.mo2").fadeOut();
+    });
+    
+    $(".icon-links.mo3 a").click(function(){
+        $(".port-pop.mo3").fadeIn();
+    });
+    $(".port-pop.mo3 .close-btn").click(function(){
+        $(".port-pop.mo3").fadeOut();
+    });
+     
+    $(".icon-links.mo4 a").click(function(){
+        $(".port-pop.mo4").fadeIn();
+    });
+    $(".port-pop.mo4 .close-btn").click(function(){
+        $(".port-pop.mo4").fadeOut();
+    });
+    
+    $(".icon-links.edit1 a").click(function(){
+        $(".port-pop.edit1").fadeIn();
+    });
+    $(".port-pop.edit1 .close-btn").click(function(){
+        $(".port-pop.edit1").fadeOut();
+    });
+     
+    $(".icon-links.edit2 a").click(function(){
+        $(".port-pop.edit2").fadeIn();
+    });
+    $(".port-pop.edit2 .close-btn").click(function(){
+        $(".port-pop.edit2").fadeOut();
+    });
+     
+    $(".icon-links.edit3 a").click(function(){
+        $(".port-pop.edit3").fadeIn();
+    });
+    $(".port-pop.edit3 .close-btn").click(function(){
+        $(".port-pop.edit3").fadeOut();
+    });
+});
